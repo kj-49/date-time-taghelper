e777dc8d2c1bf4013bc03a3d74d246f566f1d226
kj-49 <ksjames49@gmail.com>
2025-05-10 07:29:53 -0600

    feat: add custom timezone resolver option

---
+++ src/DateTimeTagHelper/Middleware/MiddlewareHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeTagHelper.Middleware;
+public static class MiddlewareHelper
+{
+    public static IApplicationBuilder UseDateTimeTagHelperMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<TimeZoneResolutionMiddleware>();
+    }
+}
---
+++ src/DateTimeTagHelper/Middleware/TimeZoneResolutionMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using DateTimeTagHelper.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeTagHelper.Middleware;
+public class TimeZoneResolutionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly DateTimeTagHelperOptions _options;
+
+    public TimeZoneResolutionMiddleware(RequestDelegate next, IOptions<DateTimeTagHelperOptions> options)
+    {
+        _next = next;
+        _options = options.Value;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (_options.CurrentTimeZoneProvider is not null)
+        {
+            var timeZone = _options.CurrentTimeZoneProvider();
+
+            if (timeZone is not null)
+            {
+                context.Items["UserTimeZone"] = timeZone;
+            }
+        }
+
+        await _next(context);
+    }
+}
+
